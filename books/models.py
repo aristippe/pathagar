92bb2304ac3c0be45f5400572faf06db0a1c5656
sinergatis <sinergatis@users.noreply.github.com>
2016-02-07 17:13:39 +0100

    Update and clean Books.model
    Update Books.model in order to provide a base for future model
    modifications, focusing on reintroducing the upload of books via the UI
    and the commands, and simplyfing validation and object saving:
    * Reintroduce "Book.book_file" as a FileField, in order to support
    uploading via UI properly.
    * Add "Book.original_path" field, as a way to provide a way of storing
    the original path to the filename in order to support uploading via
    command line (and returning the source file).
    * Reorganize field order to improve readability.
    * Update blank and null attributes of existing fields, in order to
    handle None values properly and mark fields as required/not required.
    * Update unique attributes (setting "file_sha256sum" as unique), as
    a measure for handling duplicated entries at the DB level until a more
    complex way is decided. As a result, validate_unique() has been removed
    as it was redundant.
    * Remove save(): it was attempting to re-set the cover image (by parsing
    the Epub file) each time the object was saved, not only on creation.
    * Cleanup imports.

--- books/models.py
+++ books/models.py
@@ -15,21 +15,14 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import os
+from hashlib import sha256
 
 from django.db import models
-from django.core.files import File
-from django.core.exceptions import ValidationError
-from django.conf import settings
-from django.forms.forms import NON_FIELD_ERRORS
-
-from hashlib import sha256
 
 from taggit.managers import TaggableManager  # NEW
 
 from uuidfield import UUIDField
 from langlist import langs_by_code
-from epub import Epub
 
 
 def sha256_sum(_file):  # used to generate sha256 sum of book files
@@ -107,51 +100,40 @@ class Book(models.Model):
     # https://stackoverflow.com/questions/10905674/django-how-to-save-original-filename-in-filefield
     # get the model instance you want to set the value on... set the value.. save it
 
-    # book_file = models.FileField(upload_to='books') # only_filename ?
-    book_file = models.CharField('file', max_length=1016)  # OS X 10.10 1016 chars?
-    file_sha256sum = models.CharField(max_length=64, unique=False)
+    # File related fields.
+    book_file = models.FileField(upload_to='books', null=False)
+    # TODO: OS X 10.10 1016 chars? remove max_length entirely?
+    original_path = models.CharField('file', max_length=1016)
+    file_sha256sum = models.CharField(max_length=64, unique=True)
     mimetype = models.CharField(max_length=200, null=True)
+    cover_img = models.ImageField(upload_to='covers', blank=True, null=True)
+
+    # Atom fields.
+    a_id = UUIDField('atom:id')
+    a_title = models.CharField('atom:title', max_length=200, null=False)
+    a_author = models.CharField('atom:author', max_length=200)
+    a_updated = models.DateTimeField('atom:updated', auto_now=True)
+    a_summary = models.TextField('atom:summary', blank=True, null=True)
+    a_category = models.CharField('atom:category',
+                                  max_length=200, blank=True, null=True)
+    a_rights = models.CharField('atom:rights',
+                                max_length=200, blank=True, null=True)
+
+    # Info fields.
+    dc_language = models.ForeignKey(Language, blank=True, null=True)
+    dc_publisher = models.CharField('dc:publisher',
+                                    max_length=200, blank=True, null=True)
+    dc_issued = models.CharField('dc:issued',
+                                 max_length=100, blank=True, null=True)
+    dc_identifier = models.CharField('dc:identifier',
+                                     max_length=50, blank=True, null=True,
+                                     help_text='Use ISBN for this')
+
+    # Other fields.
+    a_status = models.ForeignKey(Status, blank=False, null=False)
     time_added = models.DateTimeField(auto_now_add=True)
     tags = TaggableManager(blank=True)
     downloads = models.IntegerField(default=0)
-    a_id = UUIDField('atom:id')
-    a_status = models.ForeignKey(Status, blank=False, null=False,
-                                 default=settings.DEFAULT_BOOK_STATUS)
-    a_title = models.CharField('atom:title', max_length=200)
-    a_author = models.CharField('atom:author', max_length=200)
-    a_updated = models.DateTimeField('atom:updated', auto_now=True)
-    a_summary = models.TextField('atom:summary', blank=True)
-    a_category = models.CharField('atom:category', max_length=200, blank=True)
-    a_rights = models.CharField('atom:rights', max_length=200, blank=True)
-    dc_language = models.ForeignKey(Language, blank=True, null=True)
-    dc_publisher = models.CharField('dc:publisher', max_length=200, null=True)
-    dc_issued = models.CharField('dc:issued', max_length=100, null=True)
-    dc_identifier = models.CharField('dc:identifier', max_length=50,
-                                     help_text='Use ISBN for this', null=True)
-    cover_img = models.ImageField(blank=True, upload_to='covers')
-
-    def validate_unique(self, *args, **kwargs):
-        # if not self.file_sha256sum:
-        #     self.file_sha256sum = sha256_sum(self.book_file)
-        if (self.__class__.objects.filter(
-                file_book_file=self.file_dc_identifier).exists()):
-            raise ValidationError({
-                NON_FIELD_ERRORS:['The book already exists in the server.',]})
-
-    def save(self, *args, **kwargs):
-        if not self.cover_img:
-            if self.book_file.name.endswith('.epub'):
-                # get the cover path from the epub file
-                epub_file = Epub(self.book_file)
-                cover_path = epub_file.get_cover_image_path()
-                if cover_path is not None:
-                    cover_file = File(open(cover_path))
-                    self.cover_img.save(os.path.basename(cover_path),
-                                        cover_file)
-                epub_file.close()
-
-        # save file to books
-        super(Book, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ('-time_added',)
