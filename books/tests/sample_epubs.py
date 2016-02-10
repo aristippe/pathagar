310ff12ac953e8125a8d9ffabb2c4786af31ab2a
sinergatis <sinergatis@users.noreply.github.com>
2016-02-10 17:01:42 +0100

    Django 1.9.2 upgrade: revise settings
    Revise settings.py, appending the variables set by default when starting
    a Django project by using 'startproject', removing unused ones, and
    restructuring it a bit to make it clearer which variables are
    pathagar-specific. In turn, slightly modified local_settings.py,sample
    to conform to the main settings.py changes.
    Update sample epubs testing helper to point to the right path.

--- books/tests/sample_epubs.py
+++ books/tests/sample_epubs.py
@@ -5,8 +5,8 @@ from collections import namedtuple
 
 from django.conf import settings
 
-RSRC_DIR = os.path.abspath(os.path.join(settings.CUR_DIR,
-                                        '../resources/epubsamples'))
+RSRC_DIR = os.path.abspath(os.path.join(settings.BASE_DIR,
+                                        'resources/epubsamples'))
 SampleEpub = namedtuple('SampleEpub', ('key', 'filename', 'fullpath',
                                        'is_valid', 'has_cover'))
 
--- pathagar/settings.py
+++ pathagar/settings.py
@@ -1,95 +1,154 @@
 """
-This settings are for testing Pathagar with the Django development
-server.  It will use a SQLite database in the current directory and
-Pathagar will be available at http://127.0.0.1:8000 loopback address.
+General settings. Please copy "local_settings.py.sample" to
+"local_settings.py" in order to override deployment-specific
+variables and tailor them to the environment.
 
 For production, you should use a proper web server to deploy Django,
 serve static files, and setup a proper database.
-
 """
 
 import os
 
-# Server settings:
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SERVER_NAME = 'Pathagar'
+# -- Django settings
+# Server settings
+ALLOWED_HOSTS = []
 
-# Books settings:
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # third-party apps
+    'formtools',
+    'taggit',
+    'django_comments',
+    # pathagar apps
+    'books.apps.BooksConfig',
+]
 
-BOOKS_PER_PAGE = 50  # Number of books shown per page in the OPDS
-# catalogs and in the HTML pages.
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'pathagar.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'pathagar.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'UserAttributeSimilarityValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'MinimumLengthValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'CommonPasswordValidator'),
+    },
+    {
+        'NAME': ('django.contrib.auth.password_validation.'
+                 'NumericPasswordValidator'),
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media
+MEDIA_URL = '/static_media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_media')
+
+
+# Other settings
+LOGIN_REDIRECT_URL = '/'
+
+SITE_ID = 1
+
+INTERNAL_IPS = ['127.0.0.1']
+
+
+# -- Third-party apps settings.
+SENDFILE_BACKEND = 'sendfile.backends.development'
+
+
+# -- Pathagar settings.
+# Number of books shown per page in the OPDS catalogs and in the HTML pages.
+BOOKS_PER_PAGE = 50
 
 BOOKS_STATICS_VIA_DJANGO = True
+
 DEFAULT_BOOK_STATUS = 'Published'
 
 # Allow non logged in users to upload books
 ALLOW_PUBLIC_ADD_BOOKS = False
 
-# sendfile settings:
-
-SENDFILE_BACKEND = 'sendfile.backends.development'
-
-# Get current directory to get media and templates while developing:
-CUR_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-TIME_ZONE = 'America/Los_Angeles'
-
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-USE_I18N = True
-
-MEDIA_ROOT = os.path.join(CUR_DIR, 'static_media')
-
-MEDIA_URL = '/static_media/'
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-
-ROOT_URLCONF = 'pathagar.urls'
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-TEMPLATE_DIRS = (
-    os.path.join(CUR_DIR, 'templates'),
-)
-
-STATIC_ROOT = os.path.join(CUR_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(CUR_DIR, 'static'),
-)
-
 ALLOW_USER_COMMENTS = True
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    # third-party apps
-    'formtools',
-    'taggit',
-    'django_comments',
-    # own apps
-    'books.apps.BooksConfig'
-)
 
-LOGIN_REDIRECT_URL = '/'
-
+# -- Local settings.
 # Deployment-specific variables are imported from local_settings.py
 try:
     from local_settings import *  # NOQA
