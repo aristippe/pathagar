1aa42d4f064f653400290bb56df013e9357c8f83
sinergatis <sinergatis@users.noreply.github.com>
2016-02-10 11:24:01 +0100

    Django 1.7.11 upgrade: url names instead of views
     Replace some more instances where an url was referred to by its view
     path with the url name, as it caused problems with the unique object
     registry system introduced by the Django upgrade.

--- books/opds.py
+++ books/opds.py
@@ -68,11 +68,11 @@ def generate_nav_catalog(subsections, is_root=False):
     if is_root:
         links.append({'type': 'application/atom+xml',
                       'rel': 'self',
-                      'href': reverse('pathagar.books.views.root')})
+                      'href': reverse('root_feed')})
 
     links.append({'title': 'Home', 'type': 'application/atom+xml',
                   'rel': 'start',
-                  'href': reverse('pathagar.books.views.root')})
+                  'href': reverse('root_feed')})
 
     feed = AtomFeed(title='Pathagar Bookserver OPDS feed',
                     atom_id='pathagar:full-catalog',
@@ -144,7 +144,7 @@ def generate_catalog(request, page_obj):
     links = []
     links.append({'title': 'Home', 'type': 'application/atom+xml',
                   'rel': 'start',
-                  'href': reverse('pathagar.books.views.root')})
+                  'href': reverse('root_feed')})
 
     if page_obj.has_previous():
         previous_page = page_obj.previous_page_number()
@@ -167,14 +167,14 @@ def generate_catalog(request, page_obj):
     for book in page_obj.object_list:
         if book.cover_img:
             linklist = [{'rel': 'http://opds-spec.org/acquisition',
-                         'href': reverse('pathagar.books.views.download_book',
+                         'href': reverse('book_download',
                                          kwargs=dict(book_id=book.pk)),
                          'type': __get_mimetype(book)},
                         {'rel': 'http://opds-spec.org/cover', 'href':
                             book.cover_img.url}]
         else:
             linklist = [{'rel': 'http://opds-spec.org/acquisition',
-                         'href': reverse('pathagar.books.views.download_book',
+                         'href': reverse('book_download',
                                          kwargs=dict(book_id=book.pk)),
                          'type': __get_mimetype(book)}]
 
