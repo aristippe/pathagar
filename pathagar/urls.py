54822e4725beddd34e3edc0e64cb7deac8682577
sinergatis <sinergatis@users.noreply.github.com>
2016-02-10 16:58:54 +0100

    Django 1.9.2 upgrade: revise static and media
    Revise urls.py in order to use standard Django facilities for serving
    media and static files on a development environment (ie. when DEBUG
    is True). Replace wsgi.py with the standard Django contents in order
    to prevent problems on production servers.

--- pathagar/urls.py
+++ pathagar/urls.py
@@ -1,12 +1,11 @@
-import os
-
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login, logout
 
-from books.app_settings import BOOKS_STATICS_VIA_DJANGO
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from books import views
 from books import forms
@@ -89,15 +88,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
 
-if BOOKS_STATICS_VIA_DJANGO:
-    from django.views.static import serve
-
-    # Serve static media:
-    urlpatterns += [
-        url(r'^static_media/(?P<path>.*)$', serve,
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-
-        # Book covers:
-        url(r'^covers/(?P<path>.*)$', serve,
-            {'document_root': os.path.join(settings.MEDIA_ROOT, 'covers')}),
-    ]
+# Serve static and media files directly on development server (DEBUG).
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- pathagar/wsgi.py
+++ pathagar/wsgi.py
@@ -1,12 +1,16 @@
+"""
+WSGI config for pathagar project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
 import os
-import sys
 
-path = os.path.realpath(os.path.join(os.path.dirname(__file__), "../"))
+from django.core.wsgi import get_wsgi_application
 
-if path not in sys.path:
-    sys.path.append(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pathagar.settings")
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "pathagar.settings"
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
